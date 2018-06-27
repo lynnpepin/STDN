7c8cf172b2073ab4f92a8afae7b4f04b1cc8fce5
tpepin96 <tpepin96@gmail.com>
2018-06-27 19:28:06 -0400

    Some slight readability upgrades

--- file_loader.py
+++ file_loader.py
@@ -5,28 +5,36 @@ import json
 
 class file_loader:
     def __init__(self, config_path = "data.json"):
-        self.config = json.load(open(config_path, "r"))
+        self.config             = json.load(open(config_path, "r"))
         # how many timeslots per day (48 here)
-        self.timeslot_daynum = int(86400 / self.config["timeslot_sec"])
-        self.threshold = int(self.config["threshold"])
-        self.isVolumeLoaded = False
-        self.isFlowLoaded = False
+        self.timeslot_daynum    = int(86400 / self.config["timeslot_sec"])
+        self.threshold          = int(self.config["threshold"])
+        self.isVolumeLoaded     = False
+        self.isFlowLoaded       = False
 
 
     def load_flow(self):
-        self.flow_train = np.load(open(self.config["flow_train"], "rb"))["flow"] / self.config["flow_train_max"]
-        self.flow_test = np.load(open(self.config["flow_test"], "rb"))["flow"] / self.config["flow_train_max"]
-        self.isFlowLoaded = True
+        self.flow_train     = np.load(open(self.config["flow_train"], "rb"))["flow"] / self.config["flow_train_max"]
+        self.flow_test      = np.load(open(self.config["flow_test"], "rb"))["flow"] / self.config["flow_train_max"]
+        self.isFlowLoaded   = True
 
     def load_volume(self):
         # shape (timeslot_num, x_num, y_num, type=2)
-        self.volume_train = np.load(open(self.config["volume_train"], "rb"))["volume"] / self.config["volume_train_max"]
-        self.volume_test = np.load(open(self.config["volume_test"], "rb"))["volume"] / self.config["volume_train_max"]
+        self.volume_train   = np.load(open(self.config["volume_train"], "rb"))["volume"] / self.config["volume_train_max"]
+        self.volume_test    = np.load(open(self.config["volume_test"], "rb"))["volume"] / self.config["volume_train_max"]
         self.isVolumeLoaded = True
 
     #this function nbhd for cnn, and features for lstm, based on attention model
-    def sample_stdn(self, datatype, att_lstm_num = 3, long_term_lstm_seq_len = 3, short_term_lstm_seq_len = 7,\
-    hist_feature_daynum = 7, last_feature_num = 48, nbhd_size = 1, cnn_nbhd_size = 3):
+    def sample_stdn(self,
+                    datatype,
+                    att_lstm_num            = 3,
+                    long_term_lstm_seq_len  = 3,
+                    short_term_lstm_seq_len = 7,
+                    hist_feature_daynum     = 7,
+                    last_feature_num        = 48,
+                    nbhd_size               = 1,
+                    cnn_nbhd_size           = 3
+                   ):
         if self.isVolumeLoaded is False:
             self.load_volume()
 
@@ -48,7 +56,7 @@ class file_loader:
             raise Exception
 
 
-        cnn_att_features = []
+        cnn_att_features  = []
         lstm_att_features = []
         flow_att_features = []
         for i in range(att_lstm_num):
@@ -58,12 +66,13 @@ class file_loader:
             for j in range(long_term_lstm_seq_len):
                 cnn_att_features[i].append([])
                 flow_att_features[i].append([])
-
+        
         cnn_features = []
         flow_features = []
         for i in range(short_term_lstm_seq_len):
             cnn_features.append([])
             flow_features.append([])
+        
         short_term_lstm_features = []
         labels = []
 
@@ -76,13 +85,12 @@ class file_loader:
                 print("Now sampling at {0} timeslots.".format(t))
             for x in range(data.shape[1]):
                 for y in range(data.shape[2]):
-                    
                     #sample common (short-term) lstm
                     short_term_lstm_samples = []
                     for seqn in range(short_term_lstm_seq_len):
                         # real_t from (t - short_term_lstm_seq_len) to (t-1)
                         real_t = t - (short_term_lstm_seq_len - seqn)
-
+                        
                         #cnn features, zero_padding
                         cnn_feature = np.zeros((2*cnn_nbhd_size+1, 2*cnn_nbhd_size+1, volume_type))
                         #actual idx in data
@@ -94,14 +102,14 @@ class file_loader:
                                 #get features
                                 cnn_feature[cnn_nbhd_x - (x - cnn_nbhd_size), cnn_nbhd_y - (y - cnn_nbhd_size), :] = data[real_t, cnn_nbhd_x, cnn_nbhd_y, :]
                         cnn_features[seqn].append(cnn_feature)
-
+                        
                         #flow features, 4 type
                         flow_feature_curr_out = flow_data[0, real_t, x, y, :, :]
                         flow_feature_curr_in = flow_data[0, real_t, :, :, x, y]
                         flow_feature_last_out_to_curr = flow_data[1, real_t - 1, x, y, :, :]
                         #real_t - 1 is the time for in flow in longflow1
                         flow_feature_curr_in_from_last = flow_data[1, real_t - 1, :, :, x, y]
-
+                        
                         flow_feature = np.zeros(flow_feature_curr_in.shape+(4,))
                         
                         flow_feature[:, :, 0] = flow_feature_curr_out
@@ -117,7 +125,8 @@ class file_loader:
                                 if not (0 <= cnn_nbhd_x < data.shape[1] and 0 <= cnn_nbhd_y < data.shape[2]):
                                     continue
                                 #get features
-                                local_flow_feature[cnn_nbhd_x - (x - cnn_nbhd_size), cnn_nbhd_y - (y - cnn_nbhd_size), :] = flow_feature[cnn_nbhd_x, cnn_nbhd_y, :]
+                                local_flow_feature[cnn_nbhd_x - (x - cnn_nbhd_size),
+                                                   cnn_nbhd_y - (y - cnn_nbhd_size), :] = flow_feature[cnn_nbhd_x, cnn_nbhd_y, :]
                         flow_features[seqn].append(local_flow_feature)
 
                         #lstm features
